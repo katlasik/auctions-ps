ca28d73e17fd4ab3cbb44ed1b1ae174a5923fa73
Lucyna Paszek <lucyna.paszek@gmail.com>
2020-10-18 09:32:13 +0200

    Added User Profile.

--- src/main/java/pl/sda/auctions/controllers/HomeController.java
+++ src/main/java/pl/sda/auctions/controllers/HomeController.java
@@ -1,18 +1,23 @@
 package pl.sda.auctions.controllers;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.sda.auctions.model.User;
 import pl.sda.auctions.services.SecurityService;
+import pl.sda.auctions.services.UserService;
 
 @Controller
 public class HomeController {
 
+    private final UserService userService;
     private final SecurityService securityService;
 
-    public HomeController(SecurityService securityService) {
+    public HomeController(SecurityService securityService, UserService userService) {
         this.securityService = securityService;
+        this.userService = userService;
     }
 
     @GetMapping("/")
@@ -25,9 +30,20 @@ public class HomeController {
         if (securityService.userIsLoggedIn()) {
             return "redirect:";
         } else {
-			model.addAttribute("error", error);
-			return "login";
-		}
+            model.addAttribute("error", error);
+            return "login";
+        }
     }
+
+
+    @GetMapping("/profile")
+    public String getProfile(Model model) {
+        var email = SecurityContextHolder.getContext().getAuthentication().getName();
+        String user = userService.getUserByEmail(email).map(u -> u.getName()).orElse(" ");
+        model.addAttribute("name", user);
+        model.addAttribute("email", email);
+        return "profile";
+    }
+
 
 }
