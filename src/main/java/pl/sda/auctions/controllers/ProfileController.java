829a4efca93103fcce05a9a80168327071b171e8
Piotr Surowiec <surowiec.p@gmail.com>
2020-10-20 16:21:49 +0200

    Added Create Auction support

--- src/main/java/pl/sda/auctions/controllers/ProfileController.java
+++ src/main/java/pl/sda/auctions/controllers/ProfileController.java
@@ -4,8 +4,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import pl.sda.auctions.model.User;
 import pl.sda.auctions.services.UserService;
 
 @Controller
@@ -20,7 +18,7 @@ public class ProfileController {
     @GetMapping("/profile")
     public String getProfile(Model model) {
         var email = SecurityContextHolder.getContext().getAuthentication().getName();
-        String user = userService.getUserByEmail(email).map(User::getName).orElse(" ");
+        String user = userService.getUserByEmail(email).map(u -> u.getName()).orElse(" ");
         model.addAttribute("name", user);
         model.addAttribute("email", email);
         return "profile";
