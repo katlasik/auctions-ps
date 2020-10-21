7cfacad0f0552357f1bf6c68d771b5dc885bfdcc
Piotr Surowiec <surowiec.p@gmail.com>
2020-10-21 09:38:20 +0200

    Added Create Auction support

--- src/main/java/pl/sda/auctions/repository/UserRepository.java
+++ src/main/java/pl/sda/auctions/repository/UserRepository.java
@@ -18,6 +18,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select count(u) > 0 from User u where u.name = ?1")
     boolean checkIfNameExists(String name);
 
-
-
 }
