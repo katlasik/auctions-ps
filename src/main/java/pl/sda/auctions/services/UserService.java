8ad6751b267435a3ef22da0e2a8c79b85e936d28
Krzysztof Atlasik <krzysztof.atlasik@protonmail.com>
2020-10-25 22:40:27 +0100

    Mail support

--- src/main/java/pl/sda/auctions/services/UserService.java
+++ src/main/java/pl/sda/auctions/services/UserService.java
@@ -2,12 +2,16 @@ package pl.sda.auctions.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.sda.auctions.model.Role;
 import pl.sda.auctions.model.User;
 import pl.sda.auctions.repository.UserRepository;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.Optional;
 
 @Service
@@ -15,15 +19,17 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JavaMailSender javaMailSender;
 
     private final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public UserService(
             UserRepository userRepository,
-            PasswordEncoder passwordEncoder
-    ) {
+            PasswordEncoder passwordEncoder,
+            JavaMailSender javaMailSender) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.javaMailSender = javaMailSender;
     }
 
     public boolean checkIfUserExists(String email) {
@@ -50,6 +56,21 @@ public class UserService {
         );
         logger.info("User was created. User = {}", user);
         userRepository.save(user);
+
+
+        MimeMessage mail = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+            helper.setTo("krzysztofatlasik@gmail.com");
+            helper.setReplyTo("newsletter@codecouple.pl");
+            helper.setFrom("newsletter@codecouple.pl");
+            helper.setSubject("hello");
+            helper.setText("Hello darkness", true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        javaMailSender.send(mail);
+
     }
 
     public Optional<User> getUserByEmail(String email) {
