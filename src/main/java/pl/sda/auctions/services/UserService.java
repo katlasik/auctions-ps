b29a2b48391250304ee6453ac298700dae1869e5
Krzysztof Atlasik <krzysztof.atlasik@protonmail.com>
2020-10-26 08:44:47 +0100

    Revert "Mail support"
    This reverts commit 8ad6751b267435a3ef22da0e2a8c79b85e936d28.

--- src/main/java/pl/sda/auctions/services/UserService.java
+++ src/main/java/pl/sda/auctions/services/UserService.java
@@ -2,8 +2,6 @@ package pl.sda.auctions.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.sda.auctions.model.Role;
@@ -11,8 +9,6 @@ import pl.sda.auctions.model.User;
 import pl.sda.auctions.model.dto.ChangePasswordForm;
 import pl.sda.auctions.repository.UserRepository;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 import java.util.Optional;
 
 @Service
@@ -20,17 +16,15 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private final JavaMailSender javaMailSender;
 
     private final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public UserService(
             UserRepository userRepository,
-            PasswordEncoder passwordEncoder,
-            JavaMailSender javaMailSender) {
+            PasswordEncoder passwordEncoder
+    ) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
-        this.javaMailSender = javaMailSender;
     }
 
     public boolean checkIfUserExists(String email) {
@@ -57,21 +51,6 @@ public class UserService {
         );
         logger.info("User was created. User = {}", user);
         userRepository.save(user);
-
-
-        MimeMessage mail = javaMailSender.createMimeMessage();
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
-            helper.setTo("krzysztofatlasik@gmail.com");
-            helper.setReplyTo("newsletter@codecouple.pl");
-            helper.setFrom("newsletter@codecouple.pl");
-            helper.setSubject("hello");
-            helper.setText("Hello darkness", true);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-        javaMailSender.send(mail);
-
     }
 
     public Optional<User> getUserByEmail(String email) {
